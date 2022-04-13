68b320099cd74b897513f65ee1c9e3973ebf41bd
foreverwin_zzi <jsy11101@gmail.com>
2022-04-13 16:08:04 +0900

    fix: change BaseballGameRule.judge to add return case

--- src/main/java/baseball/domain/gamerule/BaseballGameRule.java
+++ src/main/java/baseball/domain/gamerule/BaseballGameRule.java
@@ -36,7 +36,15 @@ public class BaseballGameRule implements GameRule
     }
 
     private String judge(int strike, int ball) {
-        if (strike == 0 && ball == 0)   return "낫싱";
-        return strike == 0 ? ball + "볼" : ball + "볼 " + strike + "스트라이크";
+        String ballStr = "";
+        String strikeStr = "";
+        if (ball != 0)  ballStr = ball + "볼";
+        if (strike != 0)  strikeStr = strike + "스트라이크";
+
+        if (strike == 0 && ball == 0)  return "낫싱";
+        if (strike == 0 && ball > 0)   return ballStr;
+        if (strike > 0 && ball == 0)   return strikeStr;
+        if (strike > 0 && ball > 0)    return ballStr + " " + strikeStr;
+        return "Error";
    }
 }
