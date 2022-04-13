cf24eb42799dab91eea37f5d4d714282aeb12309
foreverwin_zzi <jsy11101@gmail.com>
2022-04-13 13:48:14 +0900

    feat: add ComputerOpponent
    Added new methods for getting opponent`s number
    - makeNumber()
    - getRandomNoDuplicate(String numberString)

---
+++ src/main/java/baseball/domain/opponent/ComputerOpponent.java
@@ -0,0 +1,30 @@
+package baseball.domain.opponent;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class ComputerOpponent implements Opponent {
+
+
+    @Override
+    public String getInput() {
+        return makeNumber();
+    }
+
+    public String makeNumber() {
+        String numberString = "";
+
+        for (int i = 0; i < 3; i++) {
+            numberString += getRandomNoDuplicate(numberString);
+        }
+        return numberString;
+    }
+
+    private String getRandomNoDuplicate(String numberString) {
+        String number = "";
+        do {
+            number = String.valueOf(Randoms.pickNumberInRange(1, 9));
+        } while (numberString.contains(number));
+
+        return number;
+    }
+}
